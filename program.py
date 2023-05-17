c651c625a0c69a7d9d568e288e4933ef58eb3653
Generekalipsa <djgamez1222@gmail.com>
2023-05-17 21:53:40 +0200

    Adding argument parsing

---
+++ program.py
@@ -0,0 +1,9 @@
+import argparse
+
+arg_parser = argparse.ArgumentParser(
+                    prog='Config Converter',
+                    description='Converts config files in xml, json and yml(yaml)')
+
+arg_parser.add_argument('pathFile1', type=open)
+arg_parser.add_argument('pathFile2', type=open)
+args = arg_parser.parse_args()
