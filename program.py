fbbc5886dba9ff67739aaa22c012c17a790a73cd
Generekalipsa <djgamez1222@gmail.com>
2023-05-20 21:59:23 +0200

    Added parsing from yaml and xml objects to json file (xml and yml not yet implemented)

--- program.py
+++ program.py
@@ -1,20 +1,20 @@
 import argparse
-import json
+import json, yaml, xmltodict
 
 arg_parser = argparse.ArgumentParser(
                     prog='Config Converter',
                     description='Converts config files in xml, json and yml(yaml)')
 
 arg_parser.add_argument('pathFile1', type=argparse.FileType('r'))
-# arg_parser.add_argument('pathFile2', type=open)
+arg_parser.add_argument('pathFile2', type=argparse.FileType('w'))
 args = arg_parser.parse_args()
 
 file_a = args.pathFile1
-# file_b = args.pathFile1
+file_b = args.pathFile1
 
 
-def validate(file, f_type):
-    match f_type:
+def validate(file, file_type):
+    match file_type:
         case "json":
             try:
                 json.load(file)
@@ -41,9 +41,46 @@ def json_load(json_file):
         return False
 
 
-file_a_type = (file_a.name.split(".", 1))[1]
-# file_b_type = (file_a.name.split(".", 1))[1]
+def json_write(object, object_type):
+    match object_type:
+        case "yml", "yaml":
+            try:
+                yaml_data = yaml.safe_load(object)
+                json_data = json.dumps(yaml_data)
+                return json_data
+            except yaml.YAMLError as err:
+                print("Error occured while parsing YAML:", err)
+        case "xml":
+            try:
+                xml_data = xmltodict.parse(object)
+                json_data = json.dumps(xml_data)
+                return json_data
+            except Exception as err:
+                print("Error occured while parsing XML:", err)
 
-match file_a_type:
-    case "json":
-        json_object = json_load(file_a)
+
+file_a_type = (file_a.name.split(".", 1))[1]
+file_b_type = (file_a.name.split(".", 1))[1]
+
+if file_a_type == file_b_type:
+    print(file_a.name, "is already in", file_a_type, "file format")
+else:
+    match file_a_type:
+        case "json":
+            json_object = json_load(file_a)
+
+
+
+    match file_b_type:
+        case "json":
+            if file_a_type == "yaml" or file_a_type == "yml":
+                json_data = json_write(yml_object, file_a_type)
+                json_file = open(file_b.name, "w")
+                json.dump(json_data, json_file)
+                json_file.close()
+            elif file_a_type == "xml":
+                json_data = json_write(xml_object, file_a_type)
+                json_file = open(file_b.name, "w")
+                json.dump(json_data, json_file)
+                json_file.close()
+
